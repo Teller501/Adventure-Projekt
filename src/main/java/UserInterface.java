01fb9552424aa644e1fc0ac8ae024b682d55912d
Teller501 <ande777j@stud.kea.dk>
2022-09-26 14:48:12 +0200

    added scanner to UserInterface

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class UserInterface {
+    Scanner keyb = new Scanner(System.in).useLocale(Locale.ENGLISH);
+    public void start() {
+
+
+    }
 }
