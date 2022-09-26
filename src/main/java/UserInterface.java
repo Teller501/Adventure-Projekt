ac1e3cd76981a5e435ba61c87846858f26feb0fe
Teller501 <ande777j@stud.kea.dk>
2022-09-26 14:58:10 +0200

    added input and interface for game

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,6 +5,23 @@ public class UserInterface {
     Scanner keyb = new Scanner(System.in).useLocale(Locale.ENGLISH);
     public void start() {
 
+        String input = "";
 
+        while(!input.equals("exit")){
+            input = keyb.nextLine().toLowerCase();
+
+            switch(input){
+                case "go north" -> System.out.println("going north");
+                case "go south" -> System.out.println("going south");
+                case "go east" -> System.out.println("going east");
+                case "go west" -> System.out.println("going west");
+                case "look" -> System.out.println("looking around");
+                case "help" -> System.out.println("display help"); //TODO: print help commands
+                case "exit" -> {
+                    System.out.println("exiting program");
+                    System.exit(1);
+                }
+            }
+        }
     }
 }
