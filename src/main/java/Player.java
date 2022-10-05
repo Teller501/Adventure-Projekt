0bd4bfa9c6abb9504a14e1f854b18542d5dd99e5
OmarKayed <omar2346@stud.kea.dk>
2022-10-05 09:17:55 +0200

    Eating food method + return healthpoints

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,9 +5,9 @@ public class Player {
     private Room currentRoom;
     private ArrayList<Item> playerInventory = new ArrayList<>();
     private double health;
-    private final double maxHealth = 100;
+    private final double maxHealth = 50;
 
-    public Player(double health){
+    public Player(double health) {
         this.health = health;
     }
 
@@ -23,23 +23,23 @@ public class Player {
         this.currentRoom = currentRoom;
     }
 
-    public boolean move(String direction){
+    public boolean move(String direction) {
         Room requestedRoom = null;
 
-        if (direction.charAt(0) == 'n'){
+        if (direction.charAt(0) == 'n') {
             requestedRoom = currentRoom.getNorthRoom();
-        }else if(direction.charAt(0) == 's'){
+        } else if (direction.charAt(0) == 's') {
             requestedRoom = currentRoom.getSouthRoom();
-        }else if(direction.charAt(0) == 'e'){
+        } else if (direction.charAt(0) == 'e') {
             requestedRoom = currentRoom.getEastRoom();
-        }else if(direction.charAt(0) == 'w'){
+        } else if (direction.charAt(0) == 'w') {
             requestedRoom = currentRoom.getWestRoom();
         }
 
-        if (requestedRoom != null){
+        if (requestedRoom != null) {
             currentRoom = requestedRoom;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -47,15 +47,16 @@ public class Player {
     public ArrayList<Item> getPlayerInventory() {
         return playerInventory;
     }
-    public void addItem(Item item){
+
+    public void addItem(Item item) {
         playerInventory.add(item);
     }
 
 
     // method for removing item from player inventory, looping through the playerInventory
-    public Item removeItem(String name){
-        for (Item item : playerInventory){
-            if (item.getName().equals(name)){
+    public Item removeItem(String name) {
+        for (Item item : playerInventory) {
+            if (item.getName().equals(name)) {
                 playerInventory.remove(item);
                 return item;
             }
@@ -64,6 +65,21 @@ public class Player {
     }
 
     // Method for eating food
+    public boolean eatingFood(String itemName) {
+        for (Item item : playerInventory) {
+            if (item instanceof Food) {
+                if (item.getName().equals(itemName)) {
+                    health += ((Food) item).getHealthPoints();
+                    return true;
+                }
 
+            }
 
+        } return false;
+    }
+
+    // Method for finding an item and returning that item
 }
+
+
+
